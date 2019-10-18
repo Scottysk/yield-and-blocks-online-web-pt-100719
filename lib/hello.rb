bbb4de196c89d2928910c40588ef21dce362e961
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-18 20:21:35 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,14 +1,22 @@
 def hello_t(array)
+<<<<<<< HEAD
   if block_given?
+=======
+>>>>>>> e38da19e0081888533f110a1b4db9a788eb0afce
   i = 0
   
   while i < array.length
   yield array[i]
   i = i + 1
   end
+<<<<<<< HEAD
   
   array
 else 
   puts "Hey! No block was given!"
   end
 end
+=======
+end
+
+>>>>>>> e38da19e0081888533f110a1b4db9a788eb0afce
